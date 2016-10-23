9ca0688b5a862c5b3ef48ab548afcf0f455626b1
Alejandro Meza <ale89.dla@gmail.com>
2016-10-23 18:20:56 -0300

    modify
    cambio de estilo

--- www/js/services.js
+++ www/js/services.js
@@ -6,29 +6,24 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
+    name: 'Test mensaje',
+    lastText: 'Este es un mensaje de prueba. Este es un mensaje de prueba',
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    name: 'Test Message',
+    lastText: 'Este es un mensaje de prueba. Este es un mensaje de prueba',
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
+    name: 'Test Message',
+    lastText: 'Este es un mensaje de prueba. Este es un mensaje de prueba',
   }, {
     id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
+    name: 'Test mensaje',
+    lastText: 'Este es un mensaje de prueba. Este es un mensaje de prueba',
   }, {
     id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    name: 'Test mensaje',
+    lastText: 'Este es un mensaje de prueba. Este es un mensaje de prueba',
   }];
 
   return {
